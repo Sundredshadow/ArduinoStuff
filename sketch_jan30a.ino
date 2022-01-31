28c770442af0368276c94ae8e5da257d34182534
Sundredshadow <54553082+Sundredshadow@users.noreply.github.com>
2022-01-31 14:03:56 -0500

    Arduino Code Moar Digital inns and outs

---
+++ sketch_jan30a.ino
@@ -0,0 +1,33 @@
+const int buttonPinOne = 2;  // the number of the pushbutton pin
+const int ledPinOne =  5;    // the number of the LED pin
+int buttonStateOne = 0;      // variable for reading the pushbutton 
+
+const int buttonPinTwo = 17;  // the number of the pushbutton pin
+const int ledPinTwo =  19;    // the number of the LED pin
+int buttonStateTwo = 0; 
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPinOne, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPinOne, INPUT);
+
+  // initialize the LED pin as an output:
+  pinMode(ledPinTwo, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPinTwo, INPUT);
+}
+void loop() {
+  // read the state of the pushbutton value:
+  buttonStateOne = digitalRead(buttonPinOne);
+  buttonStateTwo = digitalRead(buttonPinTwo);
+  if (buttonStateOne == HIGH && buttonStateTwo) {
+    // turn green LED on:
+    digitalWrite(ledPinTwo, LOW);
+    digitalWrite(ledPinOne, HIGH);       
+  } else {
+    // turn red LED on:
+    digitalWrite(ledPinOne, LOW);
+    digitalWrite(ledPinTwo, HIGH);
+  }
+}
